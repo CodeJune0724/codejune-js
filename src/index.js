7e9301f997881afba2b144d5d3c76c94b7a0148b
ZJ <1476253236@qq.com>
2022-04-04 22:24:26 +0800

    1.0.11

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import variable from "./variable.js";
 import http from "./http.js";
 import base64 from "./base64.js";
 import file from "./file.js";
+import DatabaseService from "./service/DatabaseService.js";
 import "./index.css";
 
-export default {variable, http, base64, file};
+export default {variable, http, base64, file, DatabaseService};
