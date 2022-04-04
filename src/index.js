e59423aa8b4e1c6aed2a5a385ae7c44540173230
ZJ <1476253236@qq.com>
2022-04-04 14:00:29 +0800

    优化service

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import variable from "./module/variable.js";
-import http from "./module/http.js";
-import base64 from "./module/base64.js";
-import file from "./module/file.js";
+import variable from "./variable.js";
+import http from "./http.js";
+import base64 from "./base64.js";
+import file from "./file.js";
 import "./index.css";
 
 export default {variable, http, base64, file};
