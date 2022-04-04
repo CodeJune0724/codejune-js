c94b626eecfd8c302a6ade992fc431d79325d167
ZJ <1476253236@qq.com>
2022-04-04 13:04:37 +0800

    1

--- release.bat
+++ release.bat
@@ -1,8 +1,23 @@
 @echo off
+set branch=%1
+set mainBranch=%2
 
-echo username: zj0724
-echo password: June3259123
-echo email:    1476253236@qq.com
+npm install
+npm run build
 
-call npm login
-call npm publish
+set /p isOk="手动修改进行提交:"
+if "%isOk%"=="1" (
+	git checkout %branch%
+	git merge --no-edit %mainBranch%
+)
+
+set /p isOk="解决冲突:"
+if "%isOk%"=="1" (
+	git checkout %mainBranch%
+	echo username: zj0724
+    echo password: June3259123
+    echo email:    1476253236@qq.com
+    call npm login
+    call npm publish
+	git checkout %branch%
+)
