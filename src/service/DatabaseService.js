7f0a95d4dfc57d4b9ea3f83a22d3bbb7ff0804ad
ZJ <1476253236@qq.com>
2022-04-06 09:59:52 +0800

    1.0.15

--- src/service/DatabaseService.js
+++ src/service/DatabaseService.js
@@ -1,12 +1,11 @@
 import variable from "../variable.js";
 import InfoException from "../exception/InfoException.js";
 import http from "../http.js";
+import ResponseResult from "../model/ResponseResult.js";
 
 export default class DatabaseService {
 
     url = null;
-
-    sendFunction = null;
 
     data = {
         query: {
@@ -20,82 +19,94 @@ export default class DatabaseService {
                 }
             },
 
-            response: {
-                count: null,
-                data: []
-            }
+            response: new ResponseResult()
         },
 
         save: {
             request: {},
 
-            response: {}
+            response: new ResponseResult()
         },
 
         saveList: {
             request: [],
 
-            response: {}
+            response: new ResponseResult()
         },
 
         delete: {
             request: {},
 
-            response: {}
+            response: new ResponseResult()
         },
 
         deleteList: {
             request: [],
 
-            response: {}
+            response: new ResponseResult()
         }
     };
 
-    constructor(url, sendFunction) {
+    constructor(url) {
         this.url = url;
-        this.sendFunction = sendFunction;
     }
 
     query(request) {
-        return this.$send("query", request);
+        return this.$send("query", "POST", request);
     }
 
     save(request) {
-        return this.$send("save", request);
+        return this.$send("save", "POST", request);
     }
 
     saveList(request) {
-        return this.$send("saveList", request);
+        return this.$send("saveList", "POST", request);
     }
 
     doDelete(request) {
-        return this.$send("delete", request);
+        return this.$send("delete", "POST", request);
     }
 
     deleteList(request) {
-        return this.$send("deleteList", request);
+        return this.$send("deleteList", "POST", request);
     }
 
-    $send(type, request) {
-        if (variable.isEmpty(type)) {
-            throw new InfoException("type is null");
-        }
-        if (!variable.isEmpty(request)) {
-            this.data[type].request = request;
-        }
-        let send;
-        if (!variable.isEmpty(this.sendFunction) && variable.getType(this.sendFunction) === Function) {
-            send = this.sendFunction;
-        } else {
-            send = http.send;
-        }
-        return send({
-            url: this.url + "/" + type,
-            type: "POST",
-            data: this.data[type].request
-        }, http.send).then((responseData) => {
-            this.data[type].response = responseData.data;
+    $send(methodName, type, request) {
+        return new Promise((s, e) => {
+            if (variable.isEmpty(methodName)) {
+                throw new InfoException("方法名 is null");
+            }
+            if (variable.isEmpty(type)) {
+                throw new InfoException("type is null");
+            }
+            if (variable.isEmpty(this.data[methodName])) {
+                this.data[methodName] = {
+                    request: {},
+                    response: new ResponseResult()
+                };
+            }
+            if (!variable.isEmpty(request)) {
+                this.data[methodName].request = request;
+            }
+            let requestData = {
+                url: this.url + "/" + methodName,
+                type: type,
+                data: this.data[methodName].request
+            };
+            requestData = this.$requestHandler(requestData);
+            http.send(requestData).then((responseData) => {
+                let responseDataJson = JSON.parse(responseData);
+                this.data[methodName].response.setData(responseDataJson);
+                s(this.data[methodName].response);
+            }).catch((responseData) => {
+                this.data[methodName].response = JSON.parse(responseData);
+                e(this.data[methodName].response);
+            });
         });
+    }
+
+    $requestHandler(requestData) {
+        return requestData;
     }
 
 };
