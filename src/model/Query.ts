b64e2a6a9fe4119964902467bbd31809c8724677
ZJ <1476253236@qq.com>
2022-04-11 16:50:07 +0800

    1.1.2

--- src/model/Query.ts
+++ src/model/Query.ts
@@ -1,9 +1,9 @@
 export default class Query {
     page?: number | string;
     size?: number | string;
-    filter?: object | null;
-    sort?: {
-        column: string,
-        orderBy: string
-    }
+    filter: { [key: string]: object } = {};
+    sort: {
+        column?: string,
+        orderBy?: string
+    } = {};
 }
