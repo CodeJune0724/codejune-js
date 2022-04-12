997e6f410f440cf3ccceaa4a33a3626f11bfbe34
ZJ <1476253236@qq.com>
2022-04-12 18:58:13 +0800

    1.1.9

--- src/service/DatabaseService.ts
+++ src/service/DatabaseService.ts
@@ -9,63 +9,8 @@ export default class DatabaseService<T extends BasePO> {
 
     url: string;
 
-    data: {
-        query: {
-            request: Query,
-            response: ResponseResult;
-        },
-        save: {
-            request: T | null | undefined,
-            response: ResponseResult
-        },
-        saveList: {
-            request: T[] | null,
-            response: ResponseResult
-        },
-        delete: {
-            request: T | null | undefined,
-            response: ResponseResult
-        },
-        deleteList: {
-            request: T[] | null,
-            response: ResponseResult
-        },
-        [key: string]: {
-            request: any,
-            response: ResponseResult
-        }
-    };
-
-    constructor(url: string, t?: T) {
+    constructor(url: string) {
         this.url = url;
-        this.data = {
-            query: {
-                request: new Query(),
-                response: new ResponseResult({
-                    result: {
-                        count: null,
-                        data: null
-                    }
-                })
-            },
-            save: {
-                request: t,
-                response: new ResponseResult()
-            },
-            saveList: {
-                request: null,
-                response: new ResponseResult()
-            },
-
-            delete: {
-                request: t,
-                response: new ResponseResult()
-            },
-            deleteList: {
-                request: null,
-                response: new ResponseResult()
-            }
-        };
     }
 
     query(request?: Query) {
@@ -96,29 +41,19 @@ export default class DatabaseService<T extends BasePO> {
             if (variable.isEmpty(type)) {
                 throw new InfoException("type is null");
             }
-            if (variable.isEmpty(this.data[methodName])) {
-                this.data[methodName] = {
-                    request: {},
-                    response: new ResponseResult()
-                };
-            }
-            if (request !== undefined) {
-                this.data[methodName].request = request;
-            }
-
             let requestData: any = {
                 url: this.url + "/" + methodName,
                 type: type,
-                data: this.data[methodName].request
+                data: request
             };
             requestData = this.$requestHandler(requestData);
             http.send(requestData).then((responseData) => {
                 let responseDataJson = JSON.parse(responseData);
-                this.data[methodName].response!.setData(responseDataJson);
-                if (this.data[methodName].response!.flag) {
-                    s(this.data[methodName].response);
+                let responseResult = new ResponseResult(responseDataJson);
+                if (responseResult.flag) {
+                    s(responseResult);
                 } else {
-                    e(this.data[methodName].response);
+                    e(responseResult);
                 }
             }).catch((responseData) => {
                 e(responseData);
@@ -128,18 +63,6 @@ export default class DatabaseService<T extends BasePO> {
 
     $requestHandler(requestData: object): object {
         return requestData;
-    }
-
-    $addData(methodName: string, request?: any, result?: any): void {
-        if (request === undefined) {
-            request = {};
-        }
-        this.data[methodName] = {
-            request: request,
-            response: new ResponseResult({
-                result
-            })
-        };
     }
 
 };
