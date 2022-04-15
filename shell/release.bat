d424cd4ef9ca91b51fbfe8191c0ef1fb390aba39
ZJ <1476253236@qq.com>
2022-04-15 10:59:44 +0800

    shell

--- shell/release.bat
+++ shell/release.bat
@@ -1,18 +1,16 @@
 @echo off
+
 set branch=%1
 set mainBranch=%2
 
-call npm install
-rd /s /q "dist"
-call npm run build
-
-git add .
-git reset --hard HEAD
-call npm install
-rd /s /q "dist"
-call npm run build
+cd ..
 git checkout %mainBranch%
 git merge --squash %branch%
+
+:: 前置步骤
+call npm install
+call npm run build
+
 set /p isOk="手动修改版本进行提交:"
 if "%isOk%"=="1" (
 	git checkout %branch%
@@ -21,11 +19,11 @@ if "%isOk%"=="1" (
 
 set /p isOk="解决冲突:"
 if "%isOk%"=="1" (
+    :: 后置步骤
 	git checkout %mainBranch%
 	echo username: zj0724
     echo password: June3259123
     echo email:    1476253236@qq.com
-@REM     call npm login
     call npm publish
 	git checkout %branch%
 )
