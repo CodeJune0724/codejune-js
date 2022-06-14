286786d62744d48fa1f2f30895b8f479168e32a5
ZJ <1476253236@qq.com>
2022-06-14 16:27:51 +0800

    优化filter

--- src/model/Query.ts
+++ src/model/Query.ts
@@ -1,5 +1,6 @@
 import filter from "./filter";
 import { addNull } from "../util/TypeUtil";
+import variable from "../variable";
 
 export default class Query<FILTER extends addNull<filter<T>> = null, T extends object = {}> {
     page?: number | string;
@@ -9,4 +10,8 @@ export default class Query<FILTER extends addNull<filter<T>> = null, T extends o
         column?: string,
         orderBy?: string
     };
+
+    constructor(data?: object) {
+        variable.assignment(this, data);
+    }
 };
