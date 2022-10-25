8545fb25d5260ce3c310ae327b31948a04e068d8
ZJ <1476253236@qq.com>
2022-10-25 22:52:26 +0800

    调整service

--- src/codejune.ts
+++ src/codejune.ts
@@ -1,25 +1,15 @@
 import "./css/index.css";
 
 import InfoException from "./exception/InfoException";
-
-import HttpRequest from "./model/HttpRequest";
-import httpType from "./model/httpType";
-import Query from "./model/Query";
-import ResponseResult from "./model/ResponseResult";
-import QueryResult from "./model/QueryResult";
-
-import BasePO from "./service/BasePO";
-import BaseService from "./service/BaseService";
-import DatabaseService from "./service/DatabaseService";
-
+import HttpRequest from "./http/Request";
+import httpType from "./http/type";
 import variable from "./variable";
 import http from "./http";
 import base64 from "./base64";
 import file from "./file";
 import Service from "./Service";
-
-import popupUtil from "./util/popupUtil";
-import windowUtil from "./util/windowUtil";
+import popup from "./popup";
+import window from "./window";
 
 export {
     InfoException,
@@ -29,13 +19,7 @@ export {
     http,
     base64,
     file,
-    DatabaseService,
-    Query,
-    BasePO,
-    ResponseResult,
-    BaseService,
     Service,
-    QueryResult,
-    popupUtil,
-    windowUtil
+    popup,
+    window
 };
