c5e9710e47ca7cd5617df8c85b45424cf941c0a2
ZJ <1476253236@qq.com>
2022-12-04 11:49:39 +0800

    shell

--- shell/release.sh
+++ shell/release.sh
@@ -11,17 +11,15 @@ git checkout ${mainBranch}
 git merge --squash ${branch}
 
 # 前置步骤
-rm -rf dist
-npm run build
 
 read -p "手动修改版本进行提交:" isOk
-if [ ${isOk}=="1" ]; then
+if [ "${isOk}" == "1" ]; then
     git checkout ${branch}
     git merge --no-edit ${mainBranch}
 fi
 
 read -p "检查是否存在冲突:" isOk
-if [ ${isOk}=="1" ]; then
+if [ "${isOk}" == "1" ]; then
     # 后置步骤
     npm publish
 fi
