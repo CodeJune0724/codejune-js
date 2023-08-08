3fa830fa92848314fa03aaf8681a276aacd2c95b
ZJ <1476253236@qq.com>
2023-08-08 16:55:15 +0800

    add 获取屏幕宽度

--- src/window.ts
+++ src/window.ts
@@ -1,5 +1,21 @@
 export default {
+
+    /**
+     * 获取屏幕高度
+     *
+     * @return 屏幕高度
+     * */
     getScreenHeight(): number {
         return window.screen.height;
+    },
+
+    /**
+     * 获取屏幕宽度
+     *
+     * @return 屏幕宽度
+     * */
+    getScreenWidth(): number {
+        return window.screen.width;
     }
+
 };
