66d28e681c1ae7c8e19e86dea4fd4c866f3d2c2f
ZJ <1476253236@qq.com>
2023-08-08 16:55:40 +0800

    1.2.28

--- dist/window.d.ts
+++ dist/window.d.ts
@@ -1,4 +1,15 @@
 declare const _default: {
+    /**
+     * 获取屏幕高度
+     *
+     * @return 屏幕高度
+     * */
     getScreenHeight(): number;
+    /**
+     * 获取屏幕宽度
+     *
+     * @return 屏幕宽度
+     * */
+    getScreenWidth(): number;
 };
 export default _default;
--- dist/window.js
+++ dist/window.js
@@ -1,5 +1,18 @@
 export default {
+    /**
+     * 获取屏幕高度
+     *
+     * @return 屏幕高度
+     * */
     getScreenHeight() {
         return window.screen.height;
+    },
+    /**
+     * 获取屏幕宽度
+     *
+     * @return 屏幕宽度
+     * */
+    getScreenWidth() {
+        return window.screen.width;
     }
 };
