541d4c8298c9c713ba6164d887f8fdb309294634
ZJ <1476253236@qq.com>
2024-04-07 21:38:10 +0800

    1.2.37

--- dist/codejune.js
+++ dist/codejune.js
@@ -1,5 +1,4 @@
 import "./css/index.css";
-import InfoException from "./exception/InfoException";
 import variable from "./variable";
 import Http from "./Http";
 import base64 from "./base64";
@@ -7,4 +6,4 @@ import file from "./file";
 import Service from "./Service";
 import window from "./window";
 import Websocket from "./Websocket";
-export { InfoException, variable, Http, base64, file, Service, window, Websocket };
+export { variable, Http, base64, file, Service, window, Websocket };
--- src/codejune.ts
+++ src/codejune.ts
@@ -1,6 +1,5 @@
 import "./css/index.css";
 
-import InfoException from "./exception/InfoException";
 import variable from "./variable";
 import Http from "./Http";
 import base64 from "./base64";
@@ -10,7 +9,6 @@ import window from "./window";
 import Websocket from "./Websocket";
 
 export {
-    InfoException,
     variable,
     Http,
     base64,
