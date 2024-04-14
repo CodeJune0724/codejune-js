5894360d9edf0c0b19ea48d59ff4e225e5549a3d
ZJ <1476253236@qq.com>
2024-04-14 10:28:31 +0800

    优化ServerSentEvent

--- src/ServerSentEvent.ts
+++ src/ServerSentEvent.ts
@@ -1,3 +1,15 @@
+let getData = (data: any) => {
+    if (typeof data === "string") {
+        try {
+            return JSON.parse(data);
+        } catch (e) {
+            return data;
+        }
+    } else {
+        return data;
+    }
+};
+
 export default class ServerSentEvent {
 
     private readonly url: string = "";
@@ -7,6 +19,8 @@ export default class ServerSentEvent {
     private openAction: () => void = () => {};
 
     private messageAction: { [key in string]: (data: any) => void } = {};
+
+    private errorAction: (data: any) => void = () => {};
 
     private closeAction: () => void = () => {};
 
@@ -22,6 +36,10 @@ export default class ServerSentEvent {
         this.messageAction[type] = action;
     }
 
+    onError<DATA_TYPE>(action: (data: DATA_TYPE) => void) {
+        this.errorAction = action;
+    }
+
     onClose(action: () => void) {
         this.closeAction = action;
     }
@@ -33,9 +51,12 @@ export default class ServerSentEvent {
         };
         for (let item in this.messageAction) {
             this.eventSource.addEventListener(item, (event: MessageEvent) => {
-                this.messageAction[item](event.data);
+                this.messageAction[item](getData(event.data));
             });
         }
+        this.eventSource.addEventListener("$error", (event: MessageEvent) => {
+            this.errorAction(getData(event.data));
+        });
         this.eventSource.onerror = () => {
             this.closeAction();
             this.close();
